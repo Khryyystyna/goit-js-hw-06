273ece0dc56fbb1f165247285d8083d6a4e8a10c
Khryyystyna <gayka1818@gmail.com>
2022-09-10 12:00:58 +0300

    Add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,20 @@
+
+
+const refs = {
+  changeCol: document.querySelector(".change-color"),
+  color: document.querySelector(".color"),
+  body: document.querySelector("body"),
+}
+
+function handleClick() {
+  const currentColor = getRandomHexColor();
+  refs.color.textContent = currentColor;
+  refs.body.style.background = currentColor;
+}
+
+refs.changeCol.addEventListener("click", handleClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const changeCol = document.querySelector('change-color');
-console.log(changeCol);
-
-
-const handleClick = () => {
-  changeCol.addEventListener("click", handleClick);
-};
-
-console.log(handleClick);
